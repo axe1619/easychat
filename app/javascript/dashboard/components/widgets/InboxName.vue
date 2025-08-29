b76bd28c751a82725f2645cc34cd7b50db2d0cc8
eduardo <eduardo.rodriguez.info@gmail.com>
2025-08-28 23:46:13 -0400

    agregando tag informativo para mostrar el estado de las conversaciones en el listado, front

--- app/javascript/dashboard/components/widgets/InboxName.vue
+++ app/javascript/dashboard/components/widgets/InboxName.vue
@@ -1,13 +1,24 @@
 <script>
 import { getInboxClassByType } from 'dashboard/helper/inbox';
 
+const CONVERSATION_STATUS = {
+  open: { color: "#b1e3b3" },
+  resolved: { color: "#ebcd81" },
+  pending: { color: "#d2e9ff" },
+  snoozed: { color: "#e99a9a" }
+}
+
 export default {
   props: {
     show_state:{
       type: Boolean,
       default:false
     },
-    conversation_state: {
+    show_inbox_name: {
+      type: Boolean,
+      default: true
+    },
+    conversation: {
       type: Object,
       default: null 
     },
@@ -16,12 +27,23 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return {
+      CONVERSATION_STATUS: CONVERSATION_STATUS
+    }
+  },
   computed: {
     computedInboxClass() {
       const { phone_number: phoneNumber, channel_type: type } = this.inbox;
       const classByType = getInboxClassByType(type, phoneNumber);
       return classByType;
     },
+    sorter(){
+      return this.conversation?.conversations_state
+    },
+    status(){
+      return this.conversation?.status
+    }
   },
 };
 </script>
@@ -30,21 +52,26 @@ export default {
   <div
     class="inbox--name gap-1 inline-flex items-center py-0.5 px-0 leading-3 whitespace-nowrap font-medium bg-none text-slate-600 dark:text-slate-500 text-xs my-0 mx-2.5"
   >
-    <fluent-icon
-      class="mr-0.5 rtl:ml-0.5 rtl:mr-0"
-      :icon="computedInboxClass"
-      size="12"
-    />
-    {{ inbox.name }}
-    <template v-if="conversation_state && conversation_state.name && show_state">
-      <span :style="{ backgroundColor: conversation_state.color }" class="text-xs inline-flex items-center rounded-md px-2 py-1 text-slate-900 dark:text-slate-100 font-medium">
-        {{ conversation_state.name }}
-      </span>
+    <template v-if="show_inbox_name">
+      <fluent-icon
+        class="mr-0.5 rtl:ml-0.5 rtl:mr-0"
+        :icon="computedInboxClass"
+        size="12"
+      />
+      {{ inbox.name }}
+    </template>
+    <template v-if="show_state && sorter && sorter.name">
+      <small :style="{ backgroundColor: sorter.color }" class="inline-flex items-center rounded-md px-2 py-1 text-slate-900 dark:text-slate-100 font-medium">
+        {{ sorter.name }}
+      </small>
     </template>
     <template v-else-if="show_state">
-      <span class="text-xs inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-slate-900 dark:text-slate-100 font-medium ring-1 ring-gray-500/10 ring-inset">
+      <small class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-slate-900 dark:text-slate-100 font-medium ring-1 ring-gray-500/10 ring-inset">
           no categorizada
-      </span>
+      </small>
     </template>
+    <small v-if="show_state" :style="{ backgroundColor: CONVERSATION_STATUS[status].color }" class="inline-flex items-center rounded-md px-2 py-1 text-slate-900 dark:text-slate-100 font-medium">
+      {{ $t(`CONVERSATION.STATUS.${status.toUpperCase()}`) }}
+    </small>
   </div>
 </template>
