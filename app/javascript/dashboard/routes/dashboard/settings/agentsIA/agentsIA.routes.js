becdb9fc2e94f5e4ec4734318009deca79253ff7
Rodrigo Velasco <rorito125@gmail.com>
2025-09-09 12:13:33 -0400

    FR- Agregue el boton para aniadir nuevos agentes desde las configuraciones

--- app/javascript/dashboard/routes/dashboard/settings/agentsIA/agentsIA.routes.js
+++ app/javascript/dashboard/routes/dashboard/settings/agentsIA/agentsIA.routes.js
@@ -16,6 +16,7 @@ export default {
           headerTitle: 'AGENTS_AI.HEADER_INDEX.TITLE',
           headerButtonText: 'SETTINGS.INBOXES.NEW_INBOX',
           icon: 'credit-card-person',
+          showNewButton: false,
           // newButtonRoutes: ['settings_agents_ia'],
           showBackButton,
         };
