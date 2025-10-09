eb9a82b08a8c00b98bf5027bdfba999a6c3fe8ab
eduardo <eduardo.rodriguez.info@gmail.com>
2025-10-09 15:03:07 -0400

    migracion que adiciona atributes reach y duration para las campañas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_09_23_215655) do
+ActiveRecord::Schema[7.0].define(version: 2025_10_01_201741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
@@ -237,6 +237,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_23_215655) do
     t.datetime "scheduled_at", precision: nil
     t.boolean "trigger_only_during_business_hours", default: false
     t.jsonb "additional_attributes", default: {}, null: false
+    t.integer "reach"
+    t.integer "duration"
     t.index ["account_id"], name: "index_campaigns_on_account_id"
     t.index ["campaign_status"], name: "index_campaigns_on_campaign_status"
     t.index ["campaign_type"], name: "index_campaigns_on_campaign_type"
