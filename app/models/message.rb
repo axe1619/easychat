df9d8b95918b34f812de3dd9f48ac97047919b1a
eduardo <eduardo.rodriguez.info@gmail.com>
2025-11-18 17:12:35 -0400

    agregando atributos last_activity en los eventos de crear mensajes y actualizacion de conversacion, back

--- app/models/message.rb
+++ app/models/message.rb
@@ -161,6 +161,8 @@ class Message < ApplicationRecord
       assignee_id: conversation.assignee_id,
       unread_count: conversation.unread_incoming_messages.count,
       last_activity_at: conversation.last_activity_at.to_i,
+      last_activity_incoming_at: conversation.last_activity_incoming_at.to_i,
+      last_activity_outgoing_at: conversation.last_activity_outgoing_at.to_i,
       contact_inbox: { source_id: conversation.contact_inbox.source_id }
     }
   end
