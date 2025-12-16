9fc9c84b8f1f85da6c708209c97b0694b1ee818a
eduardo <eduardo.rodriguez.info@gmail.com>
2025-12-16 14:13:34 -0400

    agregando limites para los agentes IA, back

--- app/models/account.rb
+++ app/models/account.rb
@@ -127,7 +127,8 @@ class Account < ApplicationRecord
   def usage_limits
     {
       agents: ChatwootApp.max_limit.to_i,
-      inboxes: ChatwootApp.max_limit.to_i
+      inboxes: ChatwootApp.max_limit.to_i,
+      agent_bots: ChatwootApp.max_limit.to_i
     }
   end
 
